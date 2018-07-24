4f7e3bc7642f0f884be8e70dd58a4c5b546790a8
lanocci <lanocci@example.com>
2018-07-25 08:22:00 +0900

    yarn add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,12 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
   },
+  devServer: {
+    contentBase: './dist',
+    port: 8080,
+    inline: true
+  },
+  devtool: 'source-map',
   module: {
     rules: [{
       test: /\.js$/,
