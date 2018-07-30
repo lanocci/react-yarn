5d18405eeb2f2463d73e331a0ede1d4cf82cae82
lanocci <kurn17.kkum@gmail.com>
2018-07-30 12:32:59 +0900

    debug

--- src/reddit/app.js
+++ src/reddit/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import fetch from 'isomorphic-fetch';
 
 class App extends React.Component {
@@ -31,3 +32,7 @@ class App extends React.Component {
     )
   }
 }
+
+ReactDOM.render(
+  <App />, document.getElementById('app')
+)
